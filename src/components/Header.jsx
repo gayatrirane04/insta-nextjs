cf1829fbabdcfaa6e135a8dafb6a9c5ff52b1476
Gayatri Rane <ranegayatri625@gmail.com>
2025-02-16 07:46:37 +0530

    Updated Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 'use client';
-
 import Link from 'next/link';
 import Image from 'next/image';
-// import { IoMdAddCircleOutline } from 'react-icons/io';
+import { useState } from 'react';
+import Modal from 'react-modal';
+import { IoMdAddCircleOutline } from 'react-icons/io';
 import {signIn , signOut, useSession} from 'next-auth/react';
+import {HiCamera} from 'react-icons/hi';
+import {AiOutlineClose} from 'react-icons/ai';
+
 
 export default function Header() {
    const {data : session} = useSession();
-  //  const [isOpen, setIsOpen] = useState(false);
+   const [isOpen, setIsOpen] = useState(false);
    return (
     <div className='shadow-sm border-b sticky top-0 bg-white z-30 p-3'>
         <div className='flex justify-between items-center max-w-6xl mx-auto'>
@@ -19,7 +23,6 @@ export default function Header() {
                  alt = 'instagram logo'
                 />
            </Link>
-
            <Link href='/' className='lg-hidden '>
                 <Image
                  src='/800px-Instagram_logo_2016.webp'
@@ -39,10 +42,10 @@ export default function Header() {
             
             {session ? (
                 <div className='flex gap-2 items-center'>
-                 {/* <IoMdAddCircleOutline
+                 <IoMdAddCircleOutline
                   className='text-2xl cursor-pointer transform hover:scale-125 transition duration-300 hover:text-red-600'
                   onClick={() => setIsOpen(true)}
-               /> */}
+               />
                <img
                 src={session.user.image}
                 alt={session.user.name}
@@ -58,7 +61,28 @@ export default function Header() {
               </button>
             )}
         </div>
-    </div>
+        
+          {isOpen && (
+              <Modal isOpen={isOpen} className ='max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md'
+               onRequestClose={() => setIsOpen(false)}
+               ariaHideApp={false}>
+                <div className='flex flex-col justify-center items-center h-[100%]'>
+                 <HiCamera className='text-5xl text-gray-400 cursor-pointer'/>
+                </div>
+                  <input type="text" maxLength='150'
+                  placeholder='Please enter you caption...'
+                  className='m-4 border-none text-center w-full focus:ring-0 outline-none'/>
+
+                  <button 
+                  className='w-full bg-red-600 text-white p-2 shadow-md rounded-lg hover:brightness-150 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100'>
+                      Upload Post
+                  </button>   
+                  <AiOutlineClose className='cursor-pointer absolute top-2 right-2 hover:text-red-600 transition duration-300 '
+                   onClick={() => setIsOpen(false)}
+                  />
+              </Modal>
+          )}
+    </div> 
   )
 }
 
